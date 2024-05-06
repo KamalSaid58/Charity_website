5899c8835eedfc88a92566420e965f63dd4f7be8
Yousef <yousef_758@outlook.com>
2024-05-06 10:42:20 +0300

    added buttons to donor dashboard

--- react_proj2 copy/src/Pages/Donor.js
+++ react_proj2 copy/src/Pages/Donor.js
@@ -1,24 +1,54 @@
-function Donor(){
-    return (
-        <>
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="card shadow p-3 mb-3 bg-white rounded">
-                    <div class="card-body">
-                        <h5>DonorDashboard</h5>
-                    </div>
-                    </div>
-                </div>
-            </div>    
-        
-        
-        
-        
-        
-        
-        
-        
-        </>);
+// Donor.js
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+function Donor() {
+  const navigate = useNavigate();
+
+  const handleViewOrganizationsClick = () => {
+    navigate("/ViewOrganizationDonor");
+  };
+
+  const handleViewPendingDonationsClick = () => {
+    navigate("/ViewPendingDonations");
+  };
+
+  const handleUpdateDeliveryClick = () => {
+    navigate("/ViewDeliveryDetails");
+  };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-lg mb-4 text-white w-1"
+        style={{ background: "#9F8C76" }}
+        onClick={handleViewOrganizationsClick}
+      >
+        View Organizations
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-lg mb-4 text-white w-1"
+        style={{ background: "#9F8C76" }}
+        onClick={handleViewPendingDonationsClick}
+      >
+        View Pending Donations
+      </button>
+     
+      <button
+        type="button"
+        className="btn btn-lg mb-4 text-white w-1"
+        style={{ background: "#9F8C76" }}
+        onClick={handleUpdateDeliveryClick}
+      >
+        Update Delivery Options
+      </button>
+      
+      
+    </div>
+  );
 }
 
 export default Donor;
