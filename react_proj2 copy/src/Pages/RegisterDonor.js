2b0583642b5a218bc7a9e5e113869985023ec0f0
Yousef <yousef_758@outlook.com>
2024-05-09 16:29:39 +0300

    errors now are shown when something is left empty, but pdf broke + text boxes no longer work :)

--- react_proj2 copy/src/Pages/RegisterDonor.js
+++ react_proj2 copy/src/Pages/RegisterDonor.js
@@ -1,29 +1,71 @@
-import React, { Children, useState } from "react";
+import React, { Children, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Select } from "antd";
 import UploadFile from "../Components/UploadFile";
 import "../Styles/RegisterDonor.css";
 import DonorData from "../DonorReg";
-import { Card, Flex } from "antd";
-import { Space, Button, Radio } from "antd";
+import { Card, Flex, Button } from "antd";
+import { Space, Input, Radio, notification, Tooltip } from "antd";
 
 function RegisterDonor() {
   const [fileList, setFileList] = useState([]);
+  const [api, contextHolder] = notification.useNotification();
+
+  const [gender, setGender] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [city, setCity] = useState(undefined);
+  const [selectedDonorType, setselectedDonorType] = useState(undefined);
+  const [area, setArea] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
 
   function checkNavigateTo() {
+    // const fileInput = document.getElementById("file-upload");
+    // if (!fileInput.files.length) {
+    //   isEmpty = true;
+    // }
+
+    const unfilledDataNotification = () => {
+      api.error(
+        {
+          message: "Please fill out all the boxes",
+          placement: "top",
+          duration: 3,
+        },
+        500
+      );
+    };
+
+    const successNotification = () => {
+      api.success({
+        message: "Register successful!",
+        description: "Use your email and password to login",
+        placement: "top",
+        duration: 3,
+      });
+    };
+
     const inputs = document.querySelectorAll(
       'input[type="text"], input[type="password"]'
     );
+
     let isEmpty = false;
 
-    inputs.forEach((input) => {
-      if (!input.value.trim()) {
-        isEmpty = true;
-        return;
-      }
-    });
-    const fileInput = document.getElementById("file-upload");
-    if (!fileInput.files.length) {
+    if (
+      gender === "" ||
+      firstName === "" ||
+      lastName === "" ||
+      email === "" ||
+      password === "" ||
+      city === undefined ||
+      selectedDonorType === undefined ||
+      area === "" ||
+      address === "" ||
+      phone === ""
+    ) {
       isEmpty = true;
     }
 
@@ -37,13 +79,12 @@ function RegisterDonor() {
       address: "",
       area: "",
       goveernate: "",
-      pdf: fileList,
+      // pdf: fileList,
     });
 
     if (isEmpty) {
-      alert("One of the inputs are empty");
+      unfilledDataNotification();
     } else {
-      alert("You have been accepted");
       navigate("/LoginTrial");
     }
   }
@@ -51,7 +92,46 @@ function RegisterDonor() {
   const TextBoxRow = ({ children }) => {
     return <Space>{children}</Space>;
   };
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [isInputFocused, setIsInputFocused] = useState(false);
 
+  const SingleTextBox = ({
+    formName,
+    errorMessage,
+    inputPlaceHolder,
+    value,
+    onChangeMethod,
+    textboxSize,
+  }) => {
+    const handleEmptyBox = () => {
+      setIsInputFocused(true);
+    };
+    return (
+      <Form.Item
+        name={formName}
+        rules={[{ required: true, message: errorMessage }]}
+        validateStatus={isFormSubmitted && !value ? "error" : null}
+        help={null}
+        style={{ marginBottom: 0 }}
+      >
+        <Tooltip
+          title={isFormSubmitted && !value ? errorMessage : ""}
+          // visible={(isFormSubmitted || isInputFocused) && !value}
+        >
+          <Input
+            type="text"
+            className="form-control form-control-sm"
+            id={formName}
+            placeholder={inputPlaceHolder}
+            value={value}
+            onChange={(e) => onChangeMethod(e.target.value)}
+            onBlur={handleEmptyBox}
+            style={{ width: textboxSize }}
+          />
+        </Tooltip>
+      </Form.Item>
+    );
+  };
   const CardContent = () => {
     const mo7fazatMasr = [
       { value: "Alexandria", label: "Alexandria" },
@@ -83,34 +163,39 @@ function RegisterDonor() {
       { value: "Suez", label: "Suez" },
     ];
 
-    const [gender, setGender] = useState("");
-
-    const [selectedGover, setselectedGover] = useState(undefined);
     const handleGoverChange = (value) => {
-      setselectedGover(value);
+      setCity(value);
     };
+
     const [showUploadButton, setShowUploadButton] = useState(false);
-    const [selectedDonorType, setselectedDonorType] = useState(undefined);
     const handleDonorTypeChange = (value) => {
       setselectedDonorType(value);
       setShowUploadButton(value === "Doctor" || value === "Teacher");
     };
+    const useCheckContentAndRedirect = (values) => {
+      setIsFormSubmitted(true);
+      checkNavigateTo();
+    };
+
     return (
       <Card style={{ width: 400 }} bordered="true" title="Create Donor Account">
         {/* flex below controls vertical allignment */}
-        <Flex justify="space-between" gap="middle" vertical>
+        <Flex justify="space-between" gap={15} vertical>
           <TextBoxRow>
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="FirstName"
-              placeholder="First Name"
-            />
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="LastName"
-              placeholder="Last Name"
+            <SingleTextBox
+              formName="FirstName"
+              errorMessage="Enter your first name"
+              inputPlaceHolder="First Name"
+              value={firstName}
+              onChangeMethod={setFirstName}
+            ></SingleTextBox>
+
+            <SingleTextBox
+              formName="LastName"
+              errorMessage="Enter your last name"
+              inputPlaceHolder="Last Name"
+              value={lastName}
+              onChangeMethod={setLastName}
             />
           </TextBoxRow>
 
@@ -129,79 +214,112 @@ function RegisterDonor() {
           </TextBoxRow>
 
           <TextBoxRow>
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="Email"
-              placeholder="Email"
-              style={{ width: 350 }}
+            <SingleTextBox
+              formName="Email"
+              errorMessage="Enter an Email"
+              inputPlaceHolder="Email"
+              value={email}
+              onChangeMethod={setEmail}
+              textboxSize={350}
             />
           </TextBoxRow>
           <TextBoxRow>
-            <input
-              type="Password"
-              className="form-control form-control-sm"
-              id="Password"
-              placeholder="Password"
-              style={{ width: 350 }}
-            />
-          </TextBoxRow>
-          <TextBoxRow>
-            <Select
-              className="defaultValue"
-              showSearch
-              value={selectedGover}
-              placeholder="City"
-              onChange={handleGoverChange}
-              style={{ width: 135 }}
-            >
-              {mo7fazatMasr.map((gover) => (
-                <Select.Option key={gover.value} value={gover.value}>
-                  {gover.label}
-                </Select.Option>
-              ))}
-            </Select>
-
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="Area"
-              placeholder="Area"
-              style={{ width: 206 }}
-            />
-          </TextBoxRow>
-          <TextBoxRow>
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="Address"
-              placeholder="Address"
-              style={{ width: 350 }}
-            />
-          </TextBoxRow>
-          <TextBoxRow>
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="Number"
-              placeholder="Phone Number"
-              style={{ width: 350 }}
+            <SingleTextBox
+              formName="Password"
+              errorMessage="Enter a password"
+              inputPlaceHolder="Password"
+              value={password}
+              onChangeMethod={setPassword}
+              textboxSize={350}
             />
           </TextBoxRow>
 
-          <TextBoxRow></TextBoxRow>
           <TextBoxRow>
-            <Select
-              className="defaultValue"
-              placeholder="Profession"
-              value={selectedDonorType}
-              onChange={handleDonorTypeChange}
-              style={{ width: 168 }}
+            <Form.Item
+              name={"City"}
+              rules={[{ required: true, message: "Select a city" }]}
+              validateStatus={isFormSubmitted && !city ? "error" : null}
+              help={null}
+              style={{ marginBottom: 0, display: "flex" }}
             >
-              <Select.Option value="Regular donor">None</Select.Option>
-              <Select.Option value="Doctor">Doctor</Select.Option>
-              <Select.Option value="Teacher">Teacher</Select.Option>
-            </Select>
+              <Tooltip title={isInputFocused && !city ? "Select a city" : ""}>
+                <Select
+                  type="text"
+                  className="defaultValue"
+                  showSearch
+                  value={city}
+                  placeholder="City"
+                  onChange={handleGoverChange}
+                  style={{ width: 135 }}
+                >
+                  {mo7fazatMasr.map((gover) => (
+                    <Select.Option key={gover.value} value={gover.value}>
+                      {gover.label}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Tooltip>
+            </Form.Item>
+            <SingleTextBox
+              formName="Area"
+              errorMessage="Enter your area"
+              inputPlaceHolder="Area"
+              value={area}
+              onChangeMethod={setArea}
+              textboxSize={206}
+            />
+          </TextBoxRow>
+          <TextBoxRow>
+            <SingleTextBox
+              formName="Address"
+              errorMessage="Enter your Address"
+              inputPlaceHolder="Address"
+              value={address}
+              onChangeMethod={setAddress}
+              textboxSize={350}
+            />
+          </TextBoxRow>
+          <TextBoxRow>
+            <SingleTextBox
+              formName="phoneNumber"
+              errorMessage="Enter your number"
+              inputPlaceHolder="Phone Number"
+              value={phone}
+              onChangeMethod={setPhone}
+              textboxSize={350}
+            />
+          </TextBoxRow>
+
+          <TextBoxRow>
+            <Form.Item
+              name={"Profession"}
+              rules={[{ required: true, message: "Select a profession" }]}
+              validateStatus={
+                isFormSubmitted && !selectedDonorType ? "error" : null
+              }
+              help={null}
+              style={{ marginBottom: 0 }}
+            >
+              <Tooltip
+                title={
+                  isInputFocused && !selectedDonorType
+                    ? "Select a profession"
+                    : ""
+                }
+              >
+                <Select
+                  className="defaultValue"
+                  placeholder="Profession"
+                  value={selectedDonorType}
+                  onChange={handleDonorTypeChange}
+                  style={{ width: 168 }}
+                >
+                  <Select.Option value="Regular donor">None</Select.Option>
+                  <Select.Option value="Doctor">Doctor</Select.Option>
+                  <Select.Option value="Teacher">Teacher</Select.Option>
+                </Select>
+              </Tooltip>
+            </Form.Item>
 
             {showUploadButton && (
               <div className="mb-3">
@@ -229,7 +347,8 @@ function RegisterDonor() {
               type="button"
               className="btn btn-lg mb-4 text-white"
               style={{ background: "#9F8C76" }}
-              onClick={() => checkNavigateTo()}
+              // onClick={() => checkNavigateTo()}
+              onClick={useCheckContentAndRedirect}
             >
               Register
             </button>
@@ -242,9 +361,12 @@ function RegisterDonor() {
   const navigate = useNavigate();
 
   return (
-    <Flex align="center" justify="center">
-      <CardContent />
-    </Flex>
+    <>
+      {contextHolder}
+      <Flex align="center" justify="center">
+        <CardContent />
+      </Flex>
+    </>
   );
 }
 
