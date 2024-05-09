e87c9ad73c9d5e2cb43c6086b06e3052131759e7
Yousef <yousef_758@outlook.com>
2024-05-09 12:46:43 +0300

    shape done; functionality is left

--- react_proj2 copy/src/Pages/RegisterDonor.js
+++ react_proj2 copy/src/Pages/RegisterDonor.js
@@ -5,17 +5,10 @@ import UploadFile from "../Components/UploadFile";
 import "../Styles/RegisterDonor.css";
 import DonorData from "../DonorReg";
 import { Card, Flex } from "antd";
-import { Space, Row } from "antd";
+import { Space, Button, Radio } from "antd";
 
 function RegisterDonor() {
-  const [selectedType, setSelectedType] = useState("");
-  const [showUploadButton, setShowUploadButton] = useState(false);
   const [fileList, setFileList] = useState([]);
-
-  const handleTypeChange = (value) => {
-    setSelectedType(value);
-    setShowUploadButton(value === "Doctor" || value === "Teacher");
-  };
 
   function checkNavigateTo() {
     const inputs = document.querySelectorAll(
@@ -55,26 +48,57 @@ function RegisterDonor() {
     }
   }
 
-  const cardStyle = {
-    hoverable: true,
-    title: "Register!",
-    bordered: true,
-  };
-  const flexTextBoxStyle = {
-    gap: "small",
-  };
-
   const TextBoxRow = ({ children }) => {
     return <Space>{children}</Space>;
   };
 
   const CardContent = () => {
+    const mo7fazatMasr = [
+      { value: "Alexandria", label: "Alexandria" },
+      { value: "Aswan", label: "Aswan" },
+      { value: "Asyut", label: "Asyut" },
+      { value: "Beheira", label: "Beheira" },
+      { value: "Beni Suef", label: "Beni Suef" },
+      { value: "Cairo", label: "Cairo" },
+      { value: "Dakahlia", label: "Dakahlia" },
+      { value: "Damietta", label: "Damietta" },
+      { value: "Faiyum", label: "Faiyum" },
+      { value: "Gharbia", label: "Gharbia" },
+      { value: "Giza", label: "Giza" },
+      { value: "Ismailia", label: "Ismailia" },
+      { value: "Kafr El Sheikh", label: "Kafr El Sheikh" },
+      { value: "Luxor", label: "Luxor" },
+      { value: "Matrouh", label: "Matrouh" },
+      { value: "Minya", label: "Minya" },
+      { value: "Monufia", label: "Monufia" },
+      { value: "New Valley", label: "New Valley" },
+      { value: "North Sinai", label: "North Sinai" },
+      { value: "Port Said", label: "Port Said" },
+      { value: "Qalyubia", label: "Qalyubia" },
+      { value: "Qena", label: "Qena" },
+      { value: "Red Sea", label: "Red Sea" },
+      { value: "Sharqia", label: "Sharqia" },
+      { value: "Sohag", label: "Sohag" },
+      { value: "South Sinai", label: "South Sinai" },
+      { value: "Suez", label: "Suez" },
+    ];
+
+    const [gender, setGender] = useState("");
+
+    const [selectedGover, setselectedGover] = useState(undefined);
+    const handleGoverChange = (value) => {
+      setselectedGover(value);
+    };
+    const [showUploadButton, setShowUploadButton] = useState(false);
+    const [selectedDonorType, setselectedDonorType] = useState(undefined);
+    const handleDonorTypeChange = (value) => {
+      setselectedDonorType(value);
+      setShowUploadButton(value === "Doctor" || value === "Teacher");
+    };
     return (
-      <Card style={cardStyle}>
+      <Card style={{ width: 400 }} bordered="true" title="Create Donor Account">
         {/* flex below controls vertical allignment */}
         <Flex justify="space-between" gap="middle" vertical>
-          {/* first name and last name */}
-
           <TextBoxRow>
             <input
               type="text"
@@ -91,34 +115,68 @@ function RegisterDonor() {
           </TextBoxRow>
 
           <TextBoxRow>
-            <input
-              type="text"
-              className="form-control form-control-sm"
-              id="Email"
-              placeholder="Email"
-              style={{ width: "200%" }}
-            />
+            <Radio.Group
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <Radio.Button value="start" style={{ width: 175 }}>
+                Male
+              </Radio.Button>
+              <Radio.Button value="end" style={{ width: 175 }}>
+                Female
+              </Radio.Button>
+            </Radio.Group>
           </TextBoxRow>
 
           <TextBoxRow>
             <input
               type="text"
               className="form-control form-control-sm"
-              id="Address"
-              placeholder="Address"
-              style={{ width: "50%" }}
+              id="Email"
+              placeholder="Email"
+              style={{ width: 350 }}
             />
+          </TextBoxRow>
+          <TextBoxRow>
+            <input
+              type="Password"
+              className="form-control form-control-sm"
+              id="Password"
+              placeholder="Password"
+              style={{ width: 350 }}
+            />
+          </TextBoxRow>
+          <TextBoxRow>
+            <Select
+              className="defaultValue"
+              showSearch
+              value={selectedGover}
+              placeholder="City"
+              onChange={handleGoverChange}
+              style={{ width: 135 }}
+            >
+              {mo7fazatMasr.map((gover) => (
+                <Select.Option key={gover.value} value={gover.value}>
+                  {gover.label}
+                </Select.Option>
+              ))}
+            </Select>
+
             <input
               type="text"
               className="form-control form-control-sm"
               id="Area"
               placeholder="Area"
+              style={{ width: 206 }}
             />
+          </TextBoxRow>
+          <TextBoxRow>
             <input
               type="text"
               className="form-control form-control-sm"
-              id="Governate"
-              placeholder="Governate"
+              id="Address"
+              placeholder="Address"
+              style={{ width: 350 }}
             />
           </TextBoxRow>
           <TextBoxRow>
@@ -127,24 +185,24 @@ function RegisterDonor() {
               className="form-control form-control-sm"
               id="Number"
               placeholder="Phone Number"
+              style={{ width: 350 }}
             />
           </TextBoxRow>
 
+          <TextBoxRow></TextBoxRow>
           <TextBoxRow>
-            Are you a Doctor?
-            <Form.Item>
-              <Select
-                defaultValue="Donor type"
-                className="defaultValue"
-                onChange={handleTypeChange}
-              >
-                <Select.Option value="Regular donor">
-                  Regular donor
-                </Select.Option>
-                <Select.Option value="Doctor">Doctor</Select.Option>
-                <Select.Option value="Teacher">Teacher</Select.Option>
-              </Select>
-            </Form.Item>
+            <Select
+              className="defaultValue"
+              placeholder="Profession"
+              value={selectedDonorType}
+              onChange={handleDonorTypeChange}
+              style={{ width: 168 }}
+            >
+              <Select.Option value="Regular donor">None</Select.Option>
+              <Select.Option value="Doctor">Doctor</Select.Option>
+              <Select.Option value="Teacher">Teacher</Select.Option>
+            </Select>
+
             {showUploadButton && (
               <div className="mb-3">
                 <input type="file" id="file-upload" hidden />
@@ -191,25 +249,3 @@ function RegisterDonor() {
 }
 
 export default RegisterDonor;
-
-{
-  // />
-  /* <div className="mb-3">
-<input
-  type="text"
-  className="form-control form-control-sm"
-  id="Number"
-  placeholder="Number"
-/>
-</div>
-<div className="mb-3">
-
-</div>
-
-
-
-
-<div className="divider d-flex align-items-center my-4">
-
-</div> */
-}
