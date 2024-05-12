8a5b521135d3f216f3a8b25dbe4d318f6b21c820
Yousef <yousef_758@outlook.com>
2024-05-12 16:02:24 +0300

    changed charts so they fit

--- react_proj2 copy/src/Components/PieChartDemo.js
+++ react_proj2 copy/src/Components/PieChartDemo.js
@@ -1,57 +1,70 @@
-
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'primereact/chart';
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
 
 export default function PieChartDemo() {
-    const [chartData, setChartData] = useState({});
-    const [chartOptions, setChartOptions] = useState({});
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
 
-    useEffect(() => {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const data = {
-            labels: ['Africa', 'Asia', 'Europe','North America','South America','Australia','Antarctica'],
-            datasets: [
-                {
-                    data: [10, 20, 30, 40, 50, 60,70],
-                    backgroundColor: [
-                        '#007bff', // Blue
-                        '#ffc107', // Yellow
-                        '#28a745', // Green
-                        '#dc3545', // Red
-                        '#6f42c1', // Purple
-                        '#20c997', // Teal
-                        '#fd7e14'  // Orange
-                    ],
-                    hoverBackgroundColor: [
-                        '#0056b3', // Darker Blue
-                        '#d39e00', // Darker Yellow
-                        '#218838', // Darker Green
-                        '#c82333', // Darker Red
-                        '#5a3e96', // Darker Purple
-                        '#17a2b8', // Darker Teal
-                        '#da1e2c'  // Darker Orange
-                    ]
-                }
-            ]
-        }
-        const options = {
-            plugins: {
-                legend: {
-                    labels: {
-                        usePointStyle: true
-                    }
-                }
-            }
-        };
+  useEffect(() => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const data = {
+      labels: [
+        "Bani Suef",
+        "Cairo",
+        "Benha",
+        "Alexandria",
+        "Giza",
+        "Port Said",
+        "Aswan",
+      ],
+      datasets: [
+        {
+          data: [302, 1390, 201, 1023, 583, 408, 232],
+          backgroundColor: [
+            "#007bff", // Blue
+            "#ffc107", // Yellow
+            "#28a745", // Green
+            "#dc3545", // Red
+            "#6f42c1", // Purple
+            "#20c997", // Teal
+            "#fd7e14", // Orange
+          ],
+          hoverBackgroundColor: [
+            "#0056b3", // Darker Blue
+            "#d39e00", // Darker Yellow
+            "#218838", // Darker Green
+            "#c82333", // Darker Red
+            "#5a3e96", // Darker Purple
+            "#17a2b8", // Darker Teal
+            "#da1e2c", // Darker Orange
+          ],
+        },
+      ],
+    };
+    const options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.9,
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+          },
+        },
+      },
+    };
 
-        setChartData(data);
-        setChartOptions(options);
-    }, []);
+    setChartData(data);
+    setChartOptions(options);
+  }, []);
 
-    return (
-        <div className="card flex justify-content-center">
-            <Chart type="pie" data={chartData} options={chartOptions} className="w-full md:w-30rem" />
-        </div>
-    )
+  return (
+    <div className="card flex justify-content-center items-center">
+      <Chart
+        type="pie"
+        data={chartData}
+        options={chartOptions}
+        className="w-full md:w-30rem"
+      />
+    </div>
+  );
 }
-        
--- react_proj2 copy/src/Components/VerticalBarDemo.js
+++ react_proj2 copy/src/Components/VerticalBarDemo.js
@@ -1,94 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'primereact/chart';
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
 
 export default function VerticalBarDemo() {
-    const [chartData, setChartData] = useState({});
-    const [chartOptions, setChartOptions] = useState({});
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
 
-    useEffect(() => {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-        const data = {
-            labels: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-            datasets: [
-                {
-                    label: 'Unsuccessful Donation',
-                    //backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-                    //borderColor: documentStyle.getPropertyValue('--blue-500'),
-                    backgroundColor: 'rgba(54, 162, 235, 0.6)', 
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    data: [0, 2, 4, 6, 8, 10, 12]
-                },
-                {
-                    label: 'Successful Donation',
-                    //backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-                    //borderColor: documentStyle.getPropertyValue('--pink-500'),
-                    backgroundColor: 'rgba(255, 99, 132, 0.6)', 
-                    borderColor: 'rgba(255, 99, 132, 1)', 
-                    data: [1, 3, 5, 7, 9, 13, 15]
-                }
-            ]
-        };
-        const options = {
-            maintainAspectRatio: false,
-            aspectRatio: 0.8,
-            plugins: {
-                legend: {
-                    labels: {
-                        fontColor: textColor
-                    }
-                }
+  useEffect(() => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+      "--text-color-secondary"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+    const data = {
+      labels: ["2021", "2022", "2023"],
+      datasets: [
+        {
+          label: "Unsuccessful Donation",
+          //backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          //borderColor: documentStyle.getPropertyValue('--blue-500'),
+          backgroundColor: "rgba(54, 162, 235, 0.6)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          data: [203, 405, 802],
+        },
+        {
+          label: "Successful Donation",
+          //backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+          //borderColor: documentStyle.getPropertyValue('--pink-500'),
+          backgroundColor: "rgba(255, 99, 132, 0.6)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          data: [802, 2059, 5092],
+        },
+      ],
+    };
+    const options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.9,
+      plugins: {
+        legend: {
+          labels: {
+            fontColor: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: "Year",
+            color: textColorSecondary,
+            font: {
+              weight: "bold",
             },
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Year',
-                        color: textColorSecondary,
-                        font: {
-                            weight: 'bold'
-                        }
-                    },
-                    ticks: {
-                        color: textColorSecondary,
-                        font: {
-                            weight: 500
-                        }
-                    },
-                    grid: {
-                        display: false,
-                        drawBorder: false
-                    }
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Count',
-                        color: textColorSecondary,
-                        font: {
-                            weight: 'bold'
-                        }
-                    },
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                }
-            }
-        };
+          },
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            display: false,
+            drawBorder: false,
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Count",
+            color: textColorSecondary,
+            font: {
+              weight: "bold",
+            },
+          },
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
 
-        setChartData(data);
-        setChartOptions(options);
-    }, []);
+    setChartData(data);
+    setChartOptions(options);
+  }, []);
 
-    return (
-        <div className="card">
-            <Chart type="bar" data={chartData} options={chartOptions} />
-        </div>
-    )
+  return (
+    <div className="card">
+      <Chart
+        type="bar"
+        data={chartData}
+        options={chartOptions}
+        style={{ scale: 50 }}
+      />
+    </div>
+  );
 }
